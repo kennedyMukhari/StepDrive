0dbb6574393ef99a39560dced0174dd569457112
NkweJustice <oupamapoulo@gmail.com>
2019-08-20 16:15:09 +0200

    Login validations

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { HomePage } from '../home/home';
+import { RegisterPage } from '../register/register';
+
+
 
 /**
  * Generated class for the LoginPage page.
@@ -49,21 +53,31 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, public forms: FormBuilder, public navParams: NavParams) {
 
-
     this.loginForm = this.forms.group({
-
       email: new FormControl('', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$')])),
 
-      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(10)])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(10)]))
 
-      RepeatedPassword: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(10)]))
     })
-
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  Reg(){
+   this.navCtrl.push(RegisterPage);
+  }
+
+  Home(){
+    console.log("Button clicked");
+    console.log(this.loginForm.valid);
+    
+    
+    if(this.loginForm.valid){
+      this.navCtrl.push(HomePage)
+    }
   }
 
 }
