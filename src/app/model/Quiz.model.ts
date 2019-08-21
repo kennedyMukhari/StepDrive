8bd4e7b56a0ffd84b12cb7f213c72be4333cd56d
kennedyMukhari <45757080+kennedyMukhari@users.noreply.github.com>
2019-08-21 16:33:47 +0200

    quiz

--- src/app/model/Quiz.model.ts
+++ src/app/model/Quiz.model.ts
@@ -8,46 +8,46 @@ export const Quiz: Properties[] = [
     question: ' 1 Which of these documents is issued to drivers after learning??', 
     
     options:[
-            {option: 'A.Drivers license', correct: true},
-            {option: 'B.Car receipt', correct: false},
-            {option: 'C.Residence permit',correct: false},
-            {option: 'D.Learners permit',correct: false}
+            {option: 'A. Drivers license', correct: true},
+            {option: 'B. Car receipt', correct: false},
+            {option: 'C. Residence permit',correct: false},
+            {option: 'D. Learners permit',correct: false}
         ]   
     },
     {
         question: ' 2. Which of these is a barrier to driver training?', 
         options:[
-                {option: 'A.Age', correct: true},
-                {option: 'B.Height', correct: false},
-                {option: 'C.Blood Group', correct: false},
-                {option: 'D.Genoty', correct: false}
+                {option: 'A. Age', correct: true},
+                {option: 'B. Height', correct: false},
+                {option: 'C. Blood Group', correct: false},
+                {option: 'D. Genoty', correct: false}
             ]   
         },
         {
             question: '3.What does red in traffic light means?', 
             options:[
-                    {option: 'A.Danger', correct: true},
-                    {option: 'B.Warning', correct: false},
-                    {option: 'C.Go', correct: false},
-                    {option: 'D.Vehicle at fault', correct: false}
+                    {option: 'A. Danger', correct: true},
+                    {option: 'B. Warning', correct: false},
+                    {option: 'C. Go', correct: false},
+                    {option: 'D. Vehicle at fault', correct: false}
                 ]   
             },
             {
                 question: ' 4 Which of these parts of a car prevents accident?', 
                 options:[
-                        {option: 'A.Windscreen', correct: false},
-                        {option: 'B.Seat belts', correct: true},
-                        {option: 'C.Booths', correct: false},
-                        {option: 'D.Bonnets',correct: false}
+                        {option: 'A. Windscreen', correct: false},
+                        {option: 'B. Seat belts', correct: true},
+                        {option: 'C. Booths', correct: false},
+                        {option: 'D. Bonnets',correct: false}
                     ]   
                 },
                 {
     question: ' 5.What are underage drivers called?', 
      options:[
-       {option: 'A.Little ',correct: false},
-       {option: 'B.Minors ',correct: true},
+       {option: 'A. Little ',correct: false},
+       {option: 'B. Minors ',correct: true},
        {option: 'C. Majors ',correct: false},
-       {option: 'D.Peak',correct: false}
+       {option: 'D. Peak',correct: false}
           ]   
   },
   
